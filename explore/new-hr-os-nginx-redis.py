ed18b8c5b39da2d0d2f69fd614602d91ec7d0b27
Rick Jury <rjury@sumologic.com>
2021-08-16 10:24:26 +1200

    new hr and docs

---
+++ explore/new-hr-os-nginx-redis.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import json
+
+from sumologic import SumoLogic
+
+sumo = SumoLogic(os.environ.get('SUMO_ACCESS_ID'),os.environ.get('SUMO_ACCESS_KEY'))
+
+# backup existing views
+#iews=sumo.get_explorer_views()
+#with open('hr-existing.json', 'w') as outfile:
+#    json.dump(views, outfile, indent=4)
+
+# load the new explore view
+with open('./hr-os-nginx-redis.json') as json_file:
+    data = json.load(json_file)
+
+# create the explore view
+sumo.create_explorer_view(data)
